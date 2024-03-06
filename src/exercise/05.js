f810b477cb08867be6ed9c2cf79fd24886c95e9c
ONS <onshamadkl@gmail.com>
2024-03-06 20:38:00 +0100

    style composant size color

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -15,15 +15,17 @@ const largeContainer = <div className="container container--large "
         style={{color:'black', backgroundColor:'green'}}>
         Grand conteneur vert rose texte noire</div>
 
-function Container({className, children,style}){
-      return <div className={'container '+className} children={children} style={style}></div>
+function Container({ children,style, color, size}){
+  const classSize = size ? `container ${size}` :''  
+ 
+      return <div  
+      className={classSize}   
+      style={{...style,border:'1px solid',color }}>{children}  </div>
 }
-
-
 function App() {
   return (    
     <div>
-    <Container className={'container--small'} style={{color:'white' , backgroundColor:'pink'}}>Ceci est un test</Container>
+    <Container size='small' color ='blue' style={{backgroundColor:'darkslateblue'}}>Ceci est un test</Container>
       {smallContainer}
       {mediumContainer}
       {largeContainer}
